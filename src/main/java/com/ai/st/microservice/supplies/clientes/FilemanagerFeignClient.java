3bda248f9e18411ab97b5f1f1103697228171f01
Jhon Freddy Rondon <jhonfr29@gmail.com>
2019-11-28 11:27:52 -0500

    remove openfeign client

--- src/main/java/com/ai/st/microservice/supplies/clientes/FilemanagerFeignClient.java
+++
@@ -1,8 +0,0 @@
-package com.ai.st.microservice.supplies.clientes;
-
-import org.springframework.cloud.openfeign.FeignClient;
-
-@FeignClient(name = "st-microservice-filemanager")
-public interface FilemanagerFeignClient {
-
-}
