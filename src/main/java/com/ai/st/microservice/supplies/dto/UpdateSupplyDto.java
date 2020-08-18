1c25c504f315b2bf264833f0aa08541b004b056d
shiido <jhonfr29@gmail.com>
2020-08-18 18:51:10 -0500

    feat: Add services for active/inactive supplies

---
+++ src/main/java/com/ai/st/microservice/supplies/dto/UpdateSupplyDto.java
@@ -0,0 +1,28 @@
+package com.ai.st.microservice.supplies.dto;
+
+import java.io.Serializable;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "UpdateSupplyDto")
+public class UpdateSupplyDto implements Serializable {
+
+	private static final long serialVersionUID = -1832068905400656799L;
+
+	@ApiModelProperty(required = true, notes = "State ID")
+	private Long stateId;
+
+	public UpdateSupplyDto() {
+
+	}
+
+	public Long getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(Long stateId) {
+		this.stateId = stateId;
+	}
+
+}
