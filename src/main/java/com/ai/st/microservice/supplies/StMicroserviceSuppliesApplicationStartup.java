a91dc2a82740f9e0ef64642e105b018a00339011
shiido <jhonfr29@gmail.com>
2020-09-23 13:00:02 -0500

    feat: Rename type attachment

--- src/main/java/com/ai/st/microservice/supplies/StMicroserviceSuppliesApplicationStartup.java
+++ src/main/java/com/ai/st/microservice/supplies/StMicroserviceSuppliesApplicationStartup.java
@@ -70,7 +70,7 @@ public class StMicroserviceSuppliesApplicationStartup implements ApplicationList
 
 				SupplyAttachmentTypeEntity typeSupply = new SupplyAttachmentTypeEntity();
 				typeSupply.setId(SupplyAttachmentTypeBusiness.SUPPLY_ATTACHMENT_TYPE_SUPPLY);
-				typeSupply.setName("INSUMO");
+				typeSupply.setName("ARCHIVO");
 				attachmentTypeService.createAttachmentType(typeSupply);
 
 				SupplyAttachmentTypeEntity typeFTP = new SupplyAttachmentTypeEntity();
